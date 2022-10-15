f80767f9a4b0c9feed47f1e142a3a3e8cc41a7d3
kjy <kjy5947@gmail.com>
2022-10-15 18:45:05 +0900

    #13 - repository interface 작성
    이걸로 DB에 접근

---
+++ src/main/java/kjy/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,8 @@
+package kjy.projectboard.repository;
+
+import kjy.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+
+}
